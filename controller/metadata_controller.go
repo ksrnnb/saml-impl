52e6a3c8fd49298954b88bb2fe04717d62aa078f
K. Okabe <48155865+ksrnnb@users.noreply.github.com>
2022-11-10 08:57:15 +0900

    :sparkles: store metadata

--- controller/metadata_controller.go
+++ controller/metadata_controller.go
@@ -3,15 +3,28 @@ package controller
 import (
 	"net/http"
 
+	"github.com/ksrnnb/saml/model"
 	"github.com/labstack/echo/v4"
 )
 
+const defaultCompanyID = 1
+
 // IdP のメタデータ設定ページの表示
 func Metadata(c echo.Context) error {
-	return c.Render(http.StatusOK, "metadata.html", nil)
+	m := model.FindMetadtaByCompanyID(defaultCompanyID)
+	return c.Render(http.StatusOK, "metadata.html", m)
 }
 
 // IdP から取得したメタデータの登録
+// 既に存在する場合は上書き
 func CreateMetadata(c echo.Context) error {
-	return c.String(http.StatusOK, "ok")
+	m := model.NewMetadata(
+		defaultCompanyID,
+		c.FormValue("entityID"),
+		c.FormValue("certificate"),
+		c.FormValue("ssourl"),
+	)
+	m.Save()
+
+	return c.Redirect(http.StatusFound, "/metadata")
 }
---
+++ model/metadata.go
@@ -0,0 +1,44 @@
+package model
+
+type Metadata struct {
+	ID          int
+	CompanyID   int
+	EntityID    string
+	Certificate string
+	SSOURL      string
+}
+
+var metadataRepo []*Metadata
+
+func NewMetadata(cid int, eid string, cert string, ssourl string) *Metadata {
+	return &Metadata{
+		CompanyID:   cid,
+		EntityID:    eid,
+		Certificate: cert,
+		SSOURL:      ssourl,
+	}
+}
+
+func FindMetadtaByCompanyID(cid int) *Metadata {
+	for _, m := range metadataRepo {
+		if m.CompanyID == cid {
+			return m
+		}
+	}
+	return nil
+}
+
+func (m *Metadata) Save() {
+	if m.ID == 0 {
+		m.ID = len(metadataRepo) + 1
+		metadataRepo = append(metadataRepo, m)
+		return
+	}
+
+	for i, mInRepo := range metadataRepo {
+		if m.ID == mInRepo.ID {
+			metadataRepo[i] = m
+			return
+		}
+	}
+}
